8e4e74f433ba1ae512fa3b08d206398237e8aced
Dannymane <daniel.yanko5@gmail.com>
2023-11-06 13:31:26 +0100

    Angular tour of heroes practice

---
+++ tour-of-heroes1/src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root' 
+  // The providedIn: 'root' configuration means that Angular will create a single instance of HeroService
+  //  at the root level of your application and make it available for injection to any component or service
+  //   that requests it (without providing in "providers:"). SINGLETON
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[]{
+    return HEROES;
+  }
+}
--- tour-of-heroes1/src/app/heroes/heroes.component.ts
+++ tour-of-heroes1/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +9,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
+  constructor(private heroService: HeroService){} //its good practice to keep constructor body empty
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
+
   onSelect(hero: Hero){
+    console.log("hi");
     this.selectedHero = hero;
   }
 
