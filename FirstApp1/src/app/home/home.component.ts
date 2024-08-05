cd21cee0ad09580af51424a4d8d8b903d3252f23
Dannymane <daniel.yanko5@gmail.com>
2024-08-05 15:56:28 +0200

    nwe practice

---
+++ FirstApp1/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  standalone: true,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
